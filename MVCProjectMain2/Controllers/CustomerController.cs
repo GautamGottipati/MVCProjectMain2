b15e52ef7d848afc729086417326da07341107ae
GautamGottipati <gautamrace@gmail.com>
2020-02-13 18:38:15 +0530

    Adding files

---
+++ MVCProjectMain2/Controllers/CustomerController.cs
@@ -0,0 +1,121 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using MVCProjectMain2.Models;
+
+namespace MVCProjectMain2.Controllers
+{
+    public class CustomerController : Controller
+    {
+        // GET: Customer
+        public ActionResult Index()
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Registrations.ToList());
+            }
+                
+        }
+
+        // GET: Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Registrations.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // GET: Customer/Create
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: Customer/Create
+        [HttpPost]
+        public ActionResult Create(Registration customer)
+        {
+            try
+            {
+
+                // TODO: Add insert logic here
+                using (DBModels db = new DBModels())
+                {
+                    db.Registrations.Add(customer);
+                    db.SaveChanges();
+                }
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Registrations.Where(x => x.Id == id).FirstOrDefault());
+            }
+        }
+
+        // POST: Customer/Edit/5
+        [HttpPost]
+        public ActionResult Edit(int id, Registration customer)
+        {
+            try
+            {
+                // TODO: Add update logic here
+                using (DBModels db = new DBModels())
+                {
+                    db.Entry(customer).State = EntityState.Modified;
+                    db.SaveChanges();
+                }
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+
+        // GET: Customer/Delete/5
+        public ActionResult Delete(int id)
+        {
+            using (DBModels db = new DBModels())
+            {
+                return View(db.Registrations.Where(x=> x.Id == id).FirstOrDefault());
+            }
+                
+        }
+
+        // POST: Customer/Delete/5
+        [HttpPost]
+        public ActionResult Delete(int id, Registration customer)
+        {
+            try
+            {
+                // TODO: Add delete logic here
+                using (DBModels db = new DBModels())
+                {
+                    customer = db.Registrations.Where(x => x.Id == id).FirstOrDefault();
+                    db.Registrations.Remove(customer);
+                    db.SaveChanges();
+                }
+
+                    return RedirectToAction("Index");
+            }
+            catch
+            {
+                return View();
+            }
+        }
+    }
+}
---
+++ MVCProjectMain2/CustomHTMLHelpers/TokeniserClass.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MVCProjectMain2.CustomHTMLHelpers
+{
+    public static class TokeniserClass
+    {
+        public static IHtmlString Tokenize(this HtmlHelper helper,string myid,string tags)
+        {
+            TagBuilder tb = new TagBuilder("input");
+            tb.Attributes.Add("type","text");
+            tb.Attributes.Add("class", "form-control");
+            tb.Attributes.Add("id", myid);
+            tb.Attributes.Add("value", tags);
+            return new MvcHtmlString(tb.ToString(TagRenderMode.SelfClosing));
+        }
+    }
+}
